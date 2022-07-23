b4e3b2ac24d6e7bb47db6e61de7e9700ac078c5d
Barrarroso <correodelnino@gmail.com>
2022-07-23 22:10:11 +0200

    Exercises 5.13-5.14

--- part5/blogs/src/components/BlogPost.js
+++ part5/blogs/src/components/BlogPost.js
@@ -7,11 +7,11 @@ const BlogPost = ({blog, likeBlog, deleteBlog, deletable}) => {
 
 
     return(
-        <BlogContainer>
-            {blog.title}
+        <BlogContainer className='blogContainer'>
+            {blog.title}{'\n'}
+            {blog.author}
             <Togglable showText="view" hideText="hide">
                 <br/>
-                {blog.author}
                 <p>Likes: {blog.likes} <ButtonSmall onClick={likeBlog}>Like</ButtonSmall></p>
                 <a href={blog.url} target="_blank" rel="noreferrer">{blog.url}</a>
                 { deletable ? <ButtonSmall onClick={deleteBlog}>Delete</ButtonSmall> : <></>}
@@ -22,8 +22,8 @@ const BlogPost = ({blog, likeBlog, deleteBlog, deletable}) => {
 
 BlogPost.propTypes = {
     blog: PropTypes.object.isRequired,
-    likeBlog: PropTypes.func.isRequired,
-    deleteBlog: PropTypes.func.isRequired,
+    likeBlog: PropTypes.func,
+    deleteBlog: PropTypes.func,
     deletable: PropTypes.bool
 }
 
---
+++ part5/blogs/src/components/BlogPost.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import BlogPost from './BlogPost'
+
+const testBlog = {
+    title: 'This is a test blog',
+    author: 'test author',
+    url: 'test url',
+    likes: 20,
+}
+
+describe('blog post render', () => {
+    let container
+
+    beforeEach(() => {
+        container = render(<BlogPost blog={testBlog} />).container
+    })
+    
+    test('closed blog post shows title and author', () => {
+        const div = container.querySelector('.blogContainer')
+        expect(div).toHaveTextContent('This is a test blog')
+        expect(div).toHaveTextContent('test author')
+        expect(div).not.toHaveTextContent('test url')
+        expect(div).not.toHaveTextContent(20)
+    })
+    
+    test('opening blog post shows author and url as well', async () => {
+        const user = userEvent.setup()
+        const div = container.querySelector('.blogContainer')
+        const button = screen.getByText('view')
+        await user.click(button)
+    
+        expect(div).toHaveTextContent('test url')
+        expect(div).toHaveTextContent(20)
+    })
+})
+
+
+
+test('check like button clicked twice', async () => {
+    const mockHandler = jest.fn()
+    container = render(<BlogPost blog={testBlog} likeBlog={mockHandler} />).container
+    const user = userEvent.setup()
+    const viewButton = screen.getByText('view')
+    await user.click(viewButton)
+    const likeButton = screen.getByText('Like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+    
+    expect(mockHandler.mock.calls).toHaveLength(2)
+})
