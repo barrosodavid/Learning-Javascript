c0c28dc8293c1d4b47252c1ef84a347c24d1978c
Barrarroso <correodelnino@gmail.com>
2022-09-17 17:31:24 +0200

    Refactor

--- part-extra/wristbandshop-test/src/Bundle.js
+++ part-extra/wristbandshop-test/src/Bundle.js
@@ -1,122 +1,80 @@
 import BackgroundColorPicker from "./components/BackgroundColorPicker"
 import CodeColorPicker from './components/CodeColorPicker'
+import HeroCard from "./components/HeroCard"
+import MainHeader from "./components/MainHeader"
 
 const Bundle = ({bandSrc, onURISubmit, spotifyURI, setSpotifyURI, backgroundColor, setBackgroundColor, codeColor, setCodeColor}) => {
 
- return (<div>
-    <section className="w-full px-6 pb-12 antialiased bg-white">
-    <div className="mx-auto max-w-7xl">
+ return (
+    <div>
+        <section className="w-full px-6 pb-12 mx-auto max-w-7xl file:antialiased bg-white">
+            <MainHeader></MainHeader>
 
-        <nav className="relative z-50 h-24 select-none">
-            <div className="container relative flex flex-wrap items-center justify-between h-24 mx-auto overflow-hidden font-medium border-b border-gray-200 md:overflow-visible lg:justify-center sm:px-4 md:px-2 lg:px-0">
-                <div className="flex items-center justify-start w-1/4 h-full pr-4">
-                    <a href="#_" className="inline-block py-4 md:py-0">
-                        <span className="p-1 text-xl font-black leading-none text-gray-900">soundband</span>
-                    </a>
-                </div>
-                <div className="top-0 left-0 items-start hidden w-full h-full p-4 text-sm bg-gray-900 bg-opacity-50 md:items-center md:w-3/4 md:absolute lg:text-base md:bg-transparent md:p-0 md:relative md:flex">
-                    <div className="flex-col w-full h-auto overflow-hidden bg-white rounded-lg md:bg-transparent md:overflow-visible md:rounded-none md:relative md:flex md:flex-row">
-                        <a href="#_" className="inline-flex items-center block w-auto h-16 px-6 text-xl font-black leading-none text-gray-900 md:hidden">tails<span className="text-indigo-600">.</span></a>
-                        <div className="flex flex-col items-start justify-center w-full space-x-6 text-center lg:space-x-8 md:w-2/3 md:mt-0 md:flex-row md:items-center">
-                            <a href="#_" className="inline-block w-full py-2 mx-0 ml-6 font-medium text-left text-indigo-600 md:ml-0 md:w-auto md:px-0 md:mx-2 lg:mx-3 md:text-center">Home</a>
-                            <a href="#_" className="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Cart</a>
-                            <a href="#_" className="inline-block w-full py-2 mx-0 font-medium text-left text-gray-700 md:w-auto md:px-0 md:mx-2 hover:text-indigo-600 lg:mx-3 md:text-center">Contact</a>
-                            <a href="#_" className="absolute top-0 left-0 hidden py-2 mt-6 ml-10 mr-2 text-gray-600 lg:inline-block md:mt-0 md:ml-2 lg:mx-3 md:relative">
-                                <svg className="inline w-5 h-5" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
-                            </a>
-                        </div>
-                        <div className="flex flex-col items-start justify-end w-full pt-4 md:items-center md:w-1/3 md:flex-row md:py-0">
-                            <a href="#" className="w-full px-3 py-2 mr-0 text-gray-700 md:mr-2 lg:mr-3 md:w-auto">Sign In</a>
-                            <a href="#_" className="inline-flex items-center w-full px-6 py-3 text-sm font-medium leading-4 text-white bg-indigo-600 md:px-3 md:w-auto md:rounded-full lg:px-5 hover:bg-indigo-500 focus:outline-none md:focus:ring-2 focus:ring-0 focus:ring-offset-2 focus:ring-indigo-600">Sign Up</a>
-                        </div>
-                    </div>
-                </div>
-                <div className="absolute right-0 flex flex-col items-center items-end justify-center w-10 h-10 bg-white rounded-full cursor-pointer md:hidden hover:bg-gray-100">
-                    <svg className="w-6 h-6 text-gray-700" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor" x-cloak=""><path d="M4 6h16M4 12h16M4 18h16"></path></svg>
-                    <svg className="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" x-cloak=""><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path></svg>
-                </div>
-            </div>
-        </nav>
+            <HeroCard></HeroCard>
+        </section>
 
-        <div className="container max-w-lg px-4 py-32 mx-auto mt-px text-left md:max-w-none md:text-center">
-            <h1 className="text-5xl font-extrabold leading-10 tracking-tight text-left text-gray-900 md:text-center sm:leading-none md:text-6xl lg:text-7xl"><span className="inline md:block">Wearable</span> <span className="relative mt-2 text-transparent bg-clip-text bg-gradient-to-br from-indigo-600 to-indigo-500 md:inline-block">Music</span></h1>
-            <div className="mx-auto mt-5 text-gray-500 md:mt-12 md:max-w-lg md:text-center lg:text-lg">Custom wristbands that unveil your favorite tune</div>
-            <div className="flex flex-col items-center mt-12 text-center">
-                <span className="relative inline-flex w-full md:w-auto">
-                    <a href="#_" type="button" className="inline-flex items-center justify-center w-full px-8 py-4 text-base font-bold leading-6 text-white bg-indigo-600 border border-transparent rounded-full md:w-auto hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-600">
-                        Order Now
-                    </a>
-                    <span className="absolute top-0 right-0 px-2 py-1 -mt-3 -mr-6 text-xs font-medium leading-tight text-white bg-green-400 rounded-full">from $10</span>
-                </span>
-                <a href="#_" className="mt-3 text-sm text-indigo-500">Learn More</a>
-            </div>
-        </div>
+        <section className="w-full px-8 py-16 bg-gray-100 xl:px-8">
+            <div className="max-w-5xl mx-auto">
+                <div className="flex flex-col items-center md:flex-row-reverse">
 
-    </div>
-</section>
-
-<section className="w-full px-8 py-16 bg-gray-100 xl:px-8">
-    <div className="max-w-5xl mx-auto">
-        <div className="flex flex-col items-center md:flex-row-reverse">
-
-            <div className="w-full mt-16 md:mt-0 md:w-2/5">
-                <div className="relative z-10 h-auto p-8 py-10 overflow-hidden bg-white border-b-2 border-gray-300 rounded-lg shadow-2xl px-7">
-                    <form onSubmit={onURISubmit}>
-                        <h3 className="mb-6 text-2xl font-medium text-center">Choose your song</h3>
-                        <input type="text" name="code" value={spotifyURI} onChange={(e) => setSpotifyURI(e.target.value)} className="block w-full px-4 py-3 mb-4 border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-blue-500 focus:outline-none shadow-inner shadow-gray-400" placeholder="Spotify Song URI" />
-                        <h3 className="mb-6 text-2xl font-medium text-center">Choose a line color</h3>
-                        <CodeColorPicker selectedOption={codeColor} setSelectedOption={setCodeColor}></CodeColorPicker>
-                        <h3 className="mb-6 text-2xl font-medium text-center">Choose a background color</h3>
-                        <BackgroundColorPicker colorSelected={backgroundColor} setColorSelected={setBackgroundColor}></BackgroundColorPicker>
-                        <div className="block">
-                            <button type="submit" className="w-full px-3 py-4 font-medium text-white bg-blue-600 rounded-lg">Generate</button>
-                        </div>
-                        <p className="w-full mt-4 text-sm text-center text-gray-500">Don't know where you can find a song URI? <a href="#_" className="text-blue-500 underline">More info</a></p>
-                    </form>
-                </div>
-            </div>
-
-            <div className="w-full space-y-5 md:w-3/5 md:pr-16">
-                <p className="font-medium text-blue-500 hidden md:block">Result</p>
-                    <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
-                        <div className="relative top-0 left-0">
-                            <img src={bandSrc} alt="WristBand" className="" />
+                    <div className="w-full mt-16 md:mt-0 md:w-2/5">
+                        <div className="relative z-10 h-auto p-8 py-10 overflow-hidden bg-white border-b-2 border-gray-300 rounded-lg shadow-2xl px-7">
+                            <form onSubmit={onURISubmit}>
+                                <h3 className="mb-6 text-2xl font-medium text-center">Choose your song</h3>
+                                <input type="text" name="code" value={spotifyURI} onChange={(e) => setSpotifyURI(e.target.value)} className="block w-full px-4 py-3 mb-4 border-2 border-transparent border-gray-200 rounded-lg focus:ring focus:ring-blue-500 focus:outline-none shadow-inner shadow-gray-400" placeholder="Spotify Song URI" />
+                                <h3 className="mb-6 text-2xl font-medium text-center">Choose a line color</h3>
+                                <CodeColorPicker selectedOption={codeColor} setSelectedOption={setCodeColor}></CodeColorPicker>
+                                <h3 className="mb-6 text-2xl font-medium text-center">Choose a background color</h3>
+                                <BackgroundColorPicker colorSelected={backgroundColor} setColorSelected={setBackgroundColor}></BackgroundColorPicker>
+                                <div className="block">
+                                    <button type="submit" className="w-full px-3 py-4 font-medium text-white bg-blue-600 rounded-lg">Generate</button>
+                                </div>
+                                <p className="w-full mt-4 text-sm text-center text-gray-500">Don't know where you can find a song URI? <a href="#_" className="text-blue-500 underline">More info</a></p>
+                            </form>
                         </div>
                     </div>
+
+                    <div className="w-full space-y-5 md:w-3/5 md:pr-16">
+                        <p className="font-medium text-blue-500 hidden md:block">Result</p>
+                            <div className="w-full h-auto overflow-hidden rounded-md shadow-xl sm:rounded-xl">
+                                <div className="relative top-0 left-0">
+                                    <img src={bandSrc} alt="WristBand" className="" />
+                                </div>
+                            </div>
+                    </div>
+
+                </div>
             </div>
+        </section>
 
-        </div>
-    </div>
-</section>
-
-<section className="relative py-16 bg-white min-w-screen animation-fade animation-delay">
-    <div className="container mx-auto sm:px-12 xl:px-5 text-justify">
-        <p className="text-xs font-bold text-left text-pink-500 uppercase mx-6 sm:text-center sm:text-normal sm:font-bold ">
-            Got a Question? We’ve got answers.
-        </p>
-        <h3 className="mt-1 text-2xl font-bold text-left text-gray-800 mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
-            Frequently Asked Questions
-        </h3>
-        <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How does it work?</h3>
-            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
-                The customized code of this wristband is a QR-like “scannable” tag that can be used to quickly share or access a piece of content within Spotify.
-            </p>
-        </div>
-        <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">Can I link my wristband to a playlist I made?</h3>
-            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
-                Yes, it's totally fine to have a wristband with a playlist link, as long as you enter its corresponding Spotify URI
-            </p>
-        </div>
-        <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
-            <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl"> How do you find a Spotify URI?</h3>
-            <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
-                It depends on the device you are using, that's why we have more info <a href="#_" className="underline text-blue-500"> here </a>
-            </p>
-        </div>
-    </div>
-</section>
+        <section className="relative py-16 bg-white min-w-screen animation-fade animation-delay" id="faq">
+            <div className="container mx-auto sm:px-12 xl:px-5 text-justify">
+                <p className="text-xs font-bold text-left text-pink-500 uppercase mx-6 sm:text-center sm:text-normal sm:font-bold ">
+                    Got a Question? We’ve got answers.
+                </p>
+                <h3 className="mt-1 text-2xl font-bold text-left text-gray-800 mx-6 sm:text-3xl md:text-4xl lg:text-5xl sm:text-center sm:mx-0">
+                    Frequently Asked Questions
+                </h3>
+                <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                    <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">How does it work?</h3>
+                    <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+                        The customized code of this wristband is a QR-like “scannable” tag that can be used to quickly share or access a piece of content within Spotify.
+                    </p>
+                </div>
+                <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                    <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl">Can I link my wristband to a playlist I made?</h3>
+                    <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+                        Yes, it's totally fine to have a wristband with a playlist link, as long as you enter its corresponding Spotify URI
+                    </p>
+                </div>
+                <div className="w-full px-6 py-6 mx-auto mt-10 bg-white border border-gray-200 rounded-lg sm:px-8 md:px-12 sm:py-8 sm:shadow lg:w-5/6 xl:w-2/3">
+                    <h3 className="text-lg font-bold text-purple-500 sm:text-xl md:text-2xl"> How do you find a Spotify URI?</h3>
+                    <p className="mt-2 text-base text-gray-600 sm:text-lg md:text-normal">
+                        It depends on the device you are using, that's why we have more info <a href="#_" className="underline text-blue-500"> here </a>
+                    </p>
+                </div>
+            </div>
+        </section>
 
 <section className="bg-white">
     <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
---
+++ part-extra/wristbandshop-test/src/components/HomeLogo.jsx
@@ -0,0 +1,9 @@
+export default function HomeLogo() {
+    return (
+        <div className="flex items-center justify-start w-1/4 h-full pr-4">
+            <a href="#_" className="inline-block py-4 md:py-0">
+                <span className="p-1 text-xl font-black leading-none text-gray-900">soundband</span>
+            </a>
+        </div>
+    )
+}
